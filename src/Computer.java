6a2b01455331f553286341bf59e43188f1314268
Nandhinidevi740 <annadurainandhinidevi15@gmail.com>
2023-06-13 17:16:32 +0530

    new project

---
+++ src/Computer.java
@@ -0,0 +1,12 @@
+
+public class Computer {
+	public void computerModel() {
+		System.out.println("12 gen competer advanced");
+
+	}
+
+	public static void main(String[] args) {
+		Computer c = new Computer();
+		c.computerModel();
+	}
+}
---
+++ src/Desktop.java
@@ -0,0 +1,12 @@
+
+public class Desktop extends Computer {
+private void desktopSize  () {
+	System.out.println("desktop size is 12");
+}
+public static void main(String[] args) {
+	Desktop d=new Desktop();
+	d.desktopSize();
+	d.computerModel();
+}
+
+}
